35a626cad708629fcceb98a9d26fc7412d145f6f
uri shapira <u.shapira1@gmail.com>
2018-12-17 16:58:03 +0200

    added aspirtation quiz

---
+++ Quiz/AspirationQuestion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quiz
+{
+    class AspirationQuestion
+    {
+        public int value { get; set; }
+        public string Question { get; set; }
+
+        public AspirationQuestion(string i_question, int i_value)
+        {
+            value = i_value;
+            Question = i_question;
+        }
+    }
+}
---
+++ Quiz/MaleAspirationQuiz.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quiz
+{
+    class MaleAspirationQuiz : AspirationQuiz
+    {
+
+
+        public MaleAspirationQuiz()
+        {
+
+        }
+        protected override void CalculateScore()
+        {
+            
+        }
+
+        protected override List<AspirationQuestion> FullQuestionsList()
+        {
+            List<AspirationQuestion> fullList = new List<AspirationQuestion>();
+            AspirationQuestion first = new AspirationQuestion("You have a mission in life", 7);
+            AspirationQuestion second = new AspirationQuestion("You believe in yourself", 5);
+            AspirationQuestion third = new AspirationQuestion("You know what you want to do when you grow up", 5);
+            AspirationQuestion fourth = new AspirationQuestion("You have a clear vision of your future", 5);
+            AspirationQuestion fifth = new AspirationQuestion("You find it easy to stay relaxed even when there is some pressure.", 5);
+            AspirationQuestion sixth = new AspirationQuestion("You usually do things out of sheer curiousity.", 3);
+            fullList.Add(first);
+            fullList.Add(second);
+            fullList.Add(third);
+            fullList.Add(fourth);
+            fullList.Add(fifth);
+            fullList.Add(sixth);
+            return fullList;
+        }
+    }
+}
